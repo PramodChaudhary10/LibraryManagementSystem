4c49ec93a3ab37bddc2ed985f1c7769a1d4ffb5e
Pramod <c10.promod@gmail.com>
2021-05-15 04:17:04 +0545

    add student perspective book list and add request button

--- library/views.py
+++ library/views.py
@@ -103,6 +103,10 @@ def viewbook_view(request):
     books=models.Book.objects.all()
     return render(request,'library/viewbook.html',{'books':books})
 
+def studentbook_view(request):
+    books = models.Book.objects.all()
+    return render(request, 'library/studentviewbook.html', {'books': books})
+
 
 
 
